66b212ab4ba42fb9f7d5cd3171d6c972eb1ee147
kalpanag04 <kalpanag808@gmail.com>
2022-08-02 22:35:20 +0530

    Validate Firstname Starts with Capital Letter

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace RegexPatternMatching
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Patterns patterns = new Patterns();
+            if (patterns.validateString("Kalpana"))
+            {
+                Console.WriteLine("Valid");
+            }
+            else
+                Console.WriteLine("Invalid");
+        }
+    }
+}
