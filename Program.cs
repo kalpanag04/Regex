2a79fb396db086167c424c7005cf60ae50cf917c
kalpanag04 <kalpanag808@gmail.com>
2022-08-28 21:14:58 +0530

    Validating email address

--- Program.cs
+++ Program.cs
@@ -6,13 +6,19 @@ namespace RegexPatternMatching
     {
         public static void Main(string[] args)
         {
-            Patterns patterns = new Patterns();
-            if (patterns.validateString("abc.xyl@bl.co.in"))
-            {
-                Console.WriteLine("Valid");
-            }
-            else
-                Console.WriteLine("Invalid");
+            Patterns regex = new Patterns();
+            Console.WriteLine("User registration Problem statement");
+            Console.WriteLine("*First letter should be capital.");
+            Console.WriteLine("*length should be Minimum three characters.");
+            Console.WriteLine("Enter First name :");
+            string fname = Console.ReadLine();
+            regex.CheckFirstName(fname);
+            Console.WriteLine("Enter Last name :");
+            string lname = Console.ReadLine();
+            regex.CheckLastName(lname);
+            Console.WriteLine("Enter Email id :");
+            string email = Console.ReadLine();
+            regex.CheckEmail(email);
         }
     }
 }
